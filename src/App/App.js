b7243cc7c0f446ad5052396d19b9b9a9c0831efe
Andrei-Plekhov <fundiexx@gmail.com>
2022-11-07 12:27:22 +0300

    first commit

---
+++ src/App/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Hello react</h1>
+      <p className="text">hello react text</p>
+      <select>
+        <option value="1">One</option>
+        <option value="2">two</option>
+      </select>
+      <hr/>
+        <img src="/images/corgik.gif" alt="corgi" className="corgi"/>
+    </>
+  );
+}
+
+export default App;
